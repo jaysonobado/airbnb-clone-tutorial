2bb125b5f643f6417519f7cdeac69d062f726612
Jayson Obado <jayson.obado+github@gmail.com>
2023-04-16 13:46:48 +0800

    test commit

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -17,7 +17,7 @@ const UserMenu = () => {
       <div className='flex flex-row items-center gap-3'>
         <div className='hidden md:block text-sm font-semibold py-3 px-4 rounded-fullhover:bg-neutral-100 transition cursor-pointer' 
           onClick={() => {}}>
-          Airbnb You Home
+          Airbnb You Home1
         </div>
         <div className='py-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition' 
           onClick={toggleOpen}>
